a0056aeee696fb44edd8d1a10171fcac0cfbde7f
Mike Starr <go2starr@gmail.com>
2012-02-29 18:46:54 -0600

    Add State class

--- BasicAIModule/Source/Agents/Agent.h
+++ BasicAIModule/Source/Agents/Agent.h
@@ -1,3 +1,28 @@
+/*
+ *  Agent.h 
+ */
+#pragma once
+#include "../BasicAIModule.h"
+#include "../States/State.h"
+
 class Agent 
 {
-}
+protected:
+  BWAPI::Unit &unit;
+
+  // C'tor
+  Agent(BWAPI::Unit);
+
+  // D'tor
+  ~Agent();
+
+public:
+  /*
+   * update - Called on each frame to update this agent's state and unit.
+   */
+  void update();
+
+  // attr_accessors
+  void setState(const State &state);
+  void getState(State &state);
+};
