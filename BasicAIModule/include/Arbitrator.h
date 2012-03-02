a3caf73df6ae141700882e12ae3620db67b11b2e
Mike Starr <go2starr@gmail.com>
2012-03-01 22:06:41 -0600

    Basic Arbitrator methods

--- BasicAIModule/include/Arbitrator.h
+++ BasicAIModule/include/Arbitrator.h
@@ -3,4 +3,33 @@
  */
 #pragma once
 
+#include "Managers/Manager.h"
+
+class Arbitrator
+{
+  std::set<Manager> managers;   // Bidding Managers
+  std::set<Agent> agents;       // Agents to provide
+
+public:
+  /* 
+   * update - Update calls resolve() until newly created
+   * Units are assigned, and MWTP from Managers is at equillibrium
+   */
+  void update();
+
+private:
+  /*
+   *  resolve - Transfers available Agents to Managers with highest
+   *  willingness to pay.
+   */
+  bool resolve();
+
+  /*
+   *  findNewUnits() - Creates Agents for each newly produced Unit,
+   *  and adds them to the set of managed Agents.
+   */
+  void findNewUnits();
+
+};
+
 
