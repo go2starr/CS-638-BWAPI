d9d77b7b28ae28ccf5542ee316b2d7ad0bf61c4c
Mike Starr <go2starr@gmail.com>
2012-03-01 21:26:54 -0600

    Add update method to Managers

--- BasicAIModule/include/Managers/Manager.h
+++ BasicAIModule/include/Managers/Manager.h
@@ -15,11 +15,13 @@ class Manager
   TaskQueue tasks;
   
 public:
-  /* addTask  -  Add this task to the task queue */
-  void addTask(Task &t);
+  /* update - Called on each frame */
+  virtual void update();
 
+  /* addTask  -  Add this task to the task queue */
+  virtual void addTask(Task &t);
   /*  doTask  -  Do this task without entering the queue */
-  void doTask(Task &t);
+  virtual void doTask(Task &t);
   
   /* mwtpNext  -  Value currently placed on accepting one more of this unit type */
   virtual int mwtpNext(BWAPI::UnitType &ut);
