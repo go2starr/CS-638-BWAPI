ec867c5db7d212eec90c371c8e31be760485be68
Mike Starr <go2starr@gmail.com>
2012-03-01 21:45:05 -0600

    Add event producer

---
+++ BasicAIModule/include/EventProducer/EventProducer.h
@@ -0,0 +1,11 @@
+/*
+ *  EventProducer.h  -  Analyses the game state and throws Events which are of interest
+ */
+#include "Strategizer.h"
+
+class EventProducer
+{
+  Strategizer &strategizer;
+
+
+};
