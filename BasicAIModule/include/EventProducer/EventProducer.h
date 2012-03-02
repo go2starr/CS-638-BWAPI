8696c66a742031b80e7b94996d6398b1ad6c4859
Mike Starr <go2starr@gmail.com>
2012-03-01 21:54:25 -0600

    EventProducer - hardly any change.

--- BasicAIModule/include/EventProducer/EventProducer.h
+++ BasicAIModule/include/EventProducer/EventProducer.h
@@ -1,11 +1,14 @@
 /*
  *  EventProducer.h  -  Analyses the game state and throws Events which are of interest
  */
-#include "Strategizer.h"
+#include "../Strategizer.h"
+#include "Event.h"
 
 class EventProducer
 {
   Strategizer &strategizer;
+
+  
 
 
 };
