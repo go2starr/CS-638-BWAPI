b3c87e5495f12b67e9e7a3cdfa5f198bd5c153c8
Mike Starr <go2starr@gmail.com>
2012-03-01 20:40:19 -0600

    Add MWTP and cost to manager

---
+++ BasicAIModule/include/Managers/Task.h
@@ -0,0 +1,14 @@
+/*
+ *  Task.h  - Tasks are assigned to Managers for execution.  A Task
+ *  should provide enough information for the Manager to complete.
+ */
+#pragma once
+
+typedef unsigned Priority;      // Future class Priority will provide more dynamic sorting
+
+class Task
+{
+  Priority priority;
+
+public:
+};
