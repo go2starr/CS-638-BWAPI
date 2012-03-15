22c39b2dd83f5f7f75cdc653aff45e494126e64c
Mike Starr <go2starr@gmail.com>
2012-03-15 01:50:15 -0500

    Big updates to BuildManager.  Not functional yet, but about to start testing
    More details to come on proper use, for now you can ignore
    this commit (or peek at it)

--- BasicAIModule/include/Managers/BuildManager.h
+++ BasicAIModule/include/Managers/BuildManager.h
@@ -5,10 +5,46 @@
  */
 #pragma once
 #include "Manager.h"
+#include "ConstructionManager.h"
+#include "ProductionManager.h"
+#include <queue>
+#include <stack>
+
+using std::queue;
+using std::stack;
+
+// States a req can be in
+enum BuildReqState {
+	NEW,
+	ISSUED,
+	STARTED,
+	COMPLETE
+};
+
+// Information needed to complete a build request
+typedef struct BuildReq {
+	// External
+	BWAPI::UnitType type;
+	BWAPI::TilePosition goalPosition;
+	int count;
+
+	// Internal
+	BuildReqState state;
+	BWAPI::Unit *builder; // SCV or Structure
+} BuildReq;
 
 class BuildManager : public Manager
 {
 public: 
 	void update();
 
+	void build(BWAPI::UnitType type, bool immediate=false);
+	void build(BWAPI::UnitType type, BWAPI::TilePosition goalPosition, bool immediate=false);
+
+private:
+	ConstructionManager constructionManager;
+	ProductionManager   productionManager;
+
+	queue<BuildReq> buildQueue;
+	stack<BuildReq> currentReq;  // Requests can be recursive (dependencies)
 };
--- BasicAIModule/source/Managers/BuildManager.cpp
+++ BasicAIModule/source/Managers/BuildManager.cpp
@@ -1,5 +1,156 @@
 #include "BuildManager.h"
+#include <BWTA.h>
+
+#include <set>
+using std::set;
 
 void BuildManager::update()
 {
+	/* 
+	 * TODO:  Normally, we would pass down orders to the specialized builders.
+	 *        However, to get things rolling I'm doing it here
+	 */
+	if (buildQueue.empty())
+		return;
+
+	BuildReq req = buildQueue.front();
+	BWAPI::UnitType type = req.type;
+
+	switch (req.state)
+	{
+	case NEW: 
+	{
+		// Check reqs
+		int mineralsNeeded = type.mineralPrice();
+		int gasNeeded = type.gasPrice();
+		int mineralsOwned = BWAPI::Broodwar->self()->minerals();
+		int gasOwned = BWAPI::Broodwar->self()->gas();
+
+		// Wait if we are short on resources
+		if (mineralsOwned < mineralsNeeded ||
+			gasOwned < gasNeeded) 
+		{
+			BWAPI::Broodwar->sendText("BM: %d/%d minerals %d/%d gas for %s",
+				mineralsOwned, mineralsNeeded, gasOwned, gasNeeded, type.c_str());
+			break;
+		}
+
+		// TODO: Build dependencies if some are not resolved
+		BWAPI::UnitType builderType = type.whatBuilds().first;
+		set<BWAPI::Unit*> ownedUnits = BWAPI::Broodwar->self()->getUnits();
+		bool found = false;
+		for (set<BWAPI::Unit*>::const_iterator i = ownedUnits.begin(); i != ownedUnits.end(); i++)
+		{
+			if ((*i)->getType().getID() == builderType.getID())
+				found = true;
+		}
+		if (!found)
+		{
+			BWAPI::Broodwar->sendText("!! Bad build order:  Dependency resolving not yet implemented");
+			break;
+		}
+
+		// OK - requirements met, let's build it!
+		if (!type.isAddon())
+		{
+			// Find an idle builder
+			for (set<Agent*>::iterator i = agents.begin(); i != agents.end(); i++)
+			{
+				if ((*i)->getUnit().getType().getID() == builderType.getID() &&
+					(*i)->getState() == IdleState)
+				{
+					// Found one, build it
+					if (type.isBuilding())
+						(*i)->setState(BuildingState);
+					else
+						(*i)->setState(TrainState);
+					(*i)->setUnitTypeTarget(type);
+
+					// Remember we issued the command
+					req.builder = &(*i)->getUnit();
+					req.state = ISSUED;
+
+					// DEBUG //
+					BWAPI::Broodwar->sendText("BM: Worker found, building %s", type.c_str());
+				}
+			}
+			// Wait if we didn't find a builder
+			if (req.builder == NULL)
+				break;
+		}
+		else
+		{
+			BWAPI::Broodwar->sendText("!! Bad build order: addons not yet implemented");
+		}
+		break;
+	}
+
+	case ISSUED:
+	{
+		/* We need to determine if the order has been started (i.e. resources spent) */
+		BWAPI::Unit* builder = req.builder;
+
+		if (builder->getType().isBuilding())
+		{
+			if (builder->isTraining())
+			{
+				req.state = STARTED;
+			}
+		}
+		else if (builder->getType().isWorker())
+		{
+			/*
+			 * We can't check if a builder is *really* building using ->isConstructing(),
+			 * but we can see if a structure is being built by our builder
+			 */
+			set<BWAPI::Unit*> ownedUnits = BWAPI::Broodwar->self()->getUnits();
+			for (set<BWAPI::Unit*>::const_iterator i = ownedUnits.begin(); i != ownedUnits.end(); i++)
+			{
+				if ((*i)->getBuildUnit() == builder)
+				{
+					req.state = STARTED;
+				}
+			}
+		}
+		break;
+	}
+
+	case STARTED:
+		/*
+		 * TODO:  Monitor the orders we have started, and verify them to completion.
+		 *        In the case of a killed SCV doing a build, we may want to restart the
+		 *        order.
+		 */
+		buildQueue.pop();
+
+		// DEBUG //
+		BWAPI::Broodwar->sendText("BM: Order for %s started, popping", type.c_str());
+		break;
+
+	case COMPLETE:
+		break;
+	}
+}
+
+void BuildManager::build(BWAPI::UnitType type, bool immediate)
+{
+	BWAPI::TilePosition defaultLocation = BWAPI::Broodwar->self()->getStartLocation();
+	build(type, defaultLocation, immediate);
+}
+
+void BuildManager::build(BWAPI::UnitType type, BWAPI::TilePosition goalPosition, bool immediate)
+{
+	BuildReq req;
+	// Internal
+	req.type = type;
+	req.goalPosition = goalPosition;
+	req.count = 1;
+	// External
+	req.state = NEW;
+	req.builder = NULL;
+
+	// DEBUG //
+	BWAPI::Broodwar->sendText("BM: Rcvd order for %s", type.c_str());
+
+	buildQueue.push(req);
 }
